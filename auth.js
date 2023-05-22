dcfdb634f603600f99af44171c477addc47f337e
Sagar <dev.sagartmg@gmail.com>
2023-05-22 16:47:31 +0545

    hangman and module types

---
+++ auth.js
@@ -0,0 +1,17 @@
+export const login = () => {
+    console.log("login");
+}
+
+export const signup = () => {
+    console.log("signup");
+}
+
+
+/* export default  */
+
+// module.exports = login  // default export
+
+// module.exports = {
+//     login,
+//     signup
+// }
---
+++ index.js
@@ -0,0 +1,17 @@
+/* 
+module types 
+    common js modules  // required..
+    es modules  // required..
+*/
+
+
+// const { login, signup } = require("./auth")
+// login();
+// signup();
+
+
+// const auth = require("./auth")
+import {login,signup} from "./auth.js"
+login();
+signup();
+
